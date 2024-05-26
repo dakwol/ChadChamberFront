dfe42147434f69603c94c2bac7269ceb2c7434d5
Dakwol <daniilfomenko99@gmail.com>
2024-05-26 17:37:37 +0300

    hot fix

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,10 +1,6 @@
 const apiConfig = {
     baseUrl: 'http://localhost:8002/api/',
     baseUrlMedia: 'http://localhost:8002/media/',
-    // apiKey: ''
-    // baseUrl: 'http://51.250.126.124:8099/api/',
-    // baseUrlContragent: 'http://51.250.126.124:8099/',
-    // baseUrlMedia: 'http://51.250.126.124:8099/media/',
     apiKey: ''
 }
 
